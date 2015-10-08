5c1403dcc50e64890ae4a38ff2deb640741b39e6
Matthew Collier <collier.matthew@gmail.com>
2015-10-08 14:08:13 -0400

    Add support fot httpSignatures.

--- tests/001-service.js
+++ tests/001-service.js
@@ -14,10 +14,18 @@ var identityService = config.server.baseUri + config['identity-rest'].basePath;
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 describe('identity-service', function() {
-  it('should accept a get', function(done) {
-    request.get(
+  it.only('should accept a get', function(done) {
+    // console.log(config['identity-rest'].keys[0].privateKey.privateKeyPem);
+    // console.log(config['identity-rest'].keys[0].privateKey.publicKey);
+    var privateKeyPem =
+      config['identity-rest'].keys[0].privateKey.privateKeyPem;
+    var publicKeyId = config['identity-rest'].keys[0].privateKey.publicKey;
+    sendRequest(
       {
-        url: identityService
+        url: identityService,
+        method: 'GET',
+        privateKeyPem: privateKeyPem,
+        publicKeyId: publicKeyId
       },
       function(err, res, body) {
         console.log('BODY', body);
@@ -26,3 +34,28 @@ describe('identity-service', function() {
     );
   });
 });
+
+function sendRequest(options, callback) {
+  var url = options.url;
+  var reqBody = options.identity || null;
+  var privateKeyPem = options.privateKeyPem;
+  var publicKeyId = options.publicKeyId;
+  var reqMethod = options.method;
+
+  var requestOptions = {
+    method: reqMethod,
+    url: url,
+    json: true,
+    httpSignature: {
+      key: privateKeyPem,
+      keyId: publicKeyId,
+      headers: ['date', 'host', 'request-line']
+    }
+  };
+
+  if(reqBody) {
+    requestOptions.body = reqBody;
+  }
+
+  request(requestOptions, callback);
+}
