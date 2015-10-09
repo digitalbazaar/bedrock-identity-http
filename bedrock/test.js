56dd8566de8149fc6a5baa1c0a3cf4924c38cdee
Matthew Collier <collier.matthew@gmail.com>
2015-10-09 12:38:58 -0400

    Fix-up update route.

--- bedrock/test.js
+++ bedrock/test.js
@@ -2,9 +2,58 @@
  * Copyright (c) 2015 Digital Bazaar, Inc. All rights reserved.
  */
 var bedrock = require('bedrock');
+var config = bedrock.config;
 require('bedrock-express');
 require('bedrock-requirejs');
 require('bedrock-server');
 require('../lib/api');
+
+var permissions = config.permission.permissions;
+var roles = config.permission.roles;
+
+roles['identity.admin'] = {
+  id: 'identity.administrator',
+  label: 'Identity Administrator',
+  comment: 'Role for identity administrators.',
+  sysPermission: [
+    permissions.IDENTITY_ADMIN.id,
+    permissions.IDENTITY_ACCESS.id,
+    permissions.IDENTITY_INSERT.id,
+    permissions.IDENTITY_EDIT.id,
+    permissions.IDENTITY_REMOVE.id,
+    permissions.PUBLIC_KEY_CREATE.id,
+    permissions.PUBLIC_KEY_REMOVE.id
+  ]
+};
+roles['identity.manager'] = {
+  id: 'identity.manager',
+  label: 'Identity Manager',
+  comment: 'Role for identity managers.',
+  sysPermission: [
+    permissions.IDENTITY_ADMIN.id,
+    permissions.IDENTITY_ACCESS.id,
+    permissions.IDENTITY_INSERT.id,
+    permissions.IDENTITY_EDIT.id,
+    permissions.PUBLIC_KEY_CREATE.id,
+    permissions.PUBLIC_KEY_REMOVE.id
+  ]
+};
+
+// admin role contains all permissions
+roles['admin'] = {
+  id: 'admin',
+  label: 'Administrator',
+  comment: 'Role for System Administrator.',
+  sysPermission: [].concat(roles['identity.admin'].sysPermission)
+};
+
+// default registered identity role (contains all permissions for a regular
+// identity)
+roles['identity.registered'] = {
+  id: 'identity.registered',
+  label: 'Registered Identity',
+  comment: 'Role for registered identities.',
+  sysPermission: [].concat(roles['identity.manager'].sysPermission)
+};
 
 bedrock.start();
--- tests/001-service.js
+++ tests/001-service.js
@@ -4,9 +4,11 @@
 
 'use strict';
 
+var async = require('async');
 var bedrock = require('bedrock');
 var config = bedrock.config;
 var request = require('request');
+var uuid = require('node-uuid');
 request = request.defaults({json: true});
 
 var identityService = config.server.baseUri + config['identity-rest'].basePath;
@@ -15,82 +17,262 @@ process.env.NODE_TLS_REJECT_UNAUTHORIZED = 0;
 
 describe('identity-service', function() {
 
-  it('should accept a get request for a slug', function(done) {
-    sendRequest(
-      {
-        url: identityService + '/' + config['identity-rest'].test.testUser,
-        method: 'GET'
-      },
-      function(err, res, body) {
+  describe('unauthenticated requests', function() {
+    it('should accept a request for a slug', function(done) {
+      sendRequest(
+        {
+          url: identityService + '/' +
+            config['identity-rest'].test.users.registered.name,
+          method: 'GET'
+        },
+        function(err, res, body) {
+          should.not.exist(err);
+          should.exist(body);
+          body.should.be.an('object');
+          should.exist(body.label);
+          body.label.should.be.a('string');
+          body.label.should.equal(
+            config['identity-rest'].test.users.registered.name);
+          done();
+        }
+      );
+    });
+
+    it('should respond 404-NotFound on an unknown slug', function(done) {
+      sendRequest(
+        {
+          url: identityService + '/unknownUser',
+          method: 'GET'
+        },
+        function(err, res, body) {
+          should.not.exist(err);
+          res.statusCode.should.equal(404);
+          body.should.be.an('object');
+          should.exist(body.type);
+          body.type.should.be.a('string');
+          body.type.should.equal('NotFound');
+          done();
+        }
+      );
+    });
+  });
+
+  describe('authenticated requests', function() {
+
+    it('should accept a get request for a slug', function(done) {
+      var privateKeyPem =
+        config['identity-rest'].test.users.admin.keys.privateKey.privateKeyPem;
+      var publicKeyId =
+        config['identity-rest'].test.users.admin.keys.privateKey.publicKey;
+      sendRequest(
+        {
+          url: identityService + '/' +
+            config['identity-rest'].test.users.registered.name,
+          method: 'GET',
+          privateKeyPem: privateKeyPem,
+          publicKeyId: publicKeyId
+        },
+        function(err, res, body) {
+          should.not.exist(err);
+          should.exist(body);
+          body.should.be.an('object');
+          should.exist(body.label);
+          body.label.should.be.a('string');
+          body.label.should.equal(
+            config['identity-rest'].test.users.registered.name);
+          done(err);
+        }
+      );
+    });
+
+    it('should create and lookup a new identity', function(done) {
+      var userId = uuid.v4();
+      var privateKeyPem =
+        config['identity-rest'].test.users.admin.keys.privateKey.privateKeyPem;
+      var publicKeyId =
+        config['identity-rest'].test.users.admin.keys.privateKey.publicKey;
+      async.waterfall([
+        function(callback) {
+          sendRequest(
+            {
+              url: identityService,
+              method: 'POST',
+              identity: createIdentity(userId),
+              privateKeyPem: privateKeyPem,
+              publicKeyId: publicKeyId
+            }, callback);
+        },
+        function(res, body, callback) {
+          res.statusCode.should.equal(201);
+          should.exist(body);
+          body.should.be.an('object');
+          should.exist(body.id);
+          body.id.should.be.a('string');
+          sendRequest(
+            {
+              url: body.id,
+              method: 'GET'
+            }, callback);
+        },
+        function(res, body, callback) {
+          should.exist(body);
+          body.should.be.an('object');
+          should.exist(body.label);
+          body.label.should.be.a('string');
+          body.label.should.equal(userId);
+          callback();
+        }
+      ], function(err) {
         should.not.exist(err);
-        body.should.be.an('object');
-        should.exist(body.label);
-        body.label.should.be.a('string');
-        body.label.should.equal(config['identity-rest'].test.testUser);
         done();
-      }
-    );
-  });
+      });
+    });
 
-  it('should respond 404-NotFound on an unknown slug', function(done) {
-    sendRequest(
-      {
-        url: identityService + '/unknownUser',
-        method: 'GET'
-      },
-      function(err, res, body) {
+    it.only('should return 409 on a duplicate identity', function(done) {
+      var userId = uuid.v4();
+      var privateKeyPem =
+        config['identity-rest'].test.users.admin.keys.privateKey.privateKeyPem;
+      var publicKeyId =
+        config['identity-rest'].test.users.admin.keys.privateKey.publicKey;
+      async.waterfall([
+        function(callback) {
+          sendRequest(
+            {
+              url: identityService,
+              method: 'POST',
+              identity: createIdentity(userId),
+              privateKeyPem: privateKeyPem,
+              publicKeyId: publicKeyId
+            }, callback);
+        },
+        function(res, body, callback) {
+          res.statusCode.should.equal(201);
+          sendRequest(
+            {
+              url: identityService,
+              method: 'POST',
+              identity: createIdentity(userId),
+              privateKeyPem: privateKeyPem,
+              publicKeyId: publicKeyId
+            }, callback);
+        },
+        function(res, body, callback) {
+          res.statusCode.should.equal(409);
+          should.exist(body);
+          body.should.be.an('object');
+          should.exist(body.type);
+          body.type.should.be.a('string');
+          body.type.should.equal('DuplicateIdentity');
+          should.exist(body.details);
+          body.details.should.be.an('object');
+          should.exist(body.details.identity);
+          body.details.identity.should.be.a('string');
+          callback();
+        }
+      ], function(err) {
         should.not.exist(err);
-        res.statusCode.should.equal(404);
-        body.should.be.an('object');
-        should.exist(body.type);
-        body.type.should.be.a('string');
-        body.type.should.equal('NotFound');
         done();
-      }
-    );
+      });
+    });
   });
 
-  it.skip('should accept request with an httpSignature', function(done) {
-    // console.log(config['identity-rest'].keys[0].privateKey.privateKeyPem);
-    // console.log(config['identity-rest'].keys[0].privateKey.publicKey);
-    var privateKeyPem =
-      config['identity-rest'].keys[0].privateKey.privateKeyPem;
-    var publicKeyId = config['identity-rest'].keys[0].privateKey.publicKey;
-    sendRequest(
-      {
-        url: identityService,
-        method: 'GET',
-        privateKeyPem: privateKeyPem,
-        publicKeyId: publicKeyId
-      },
-      function(err, res, body) {
-        console.log('BODY', body);
-        done(err);
-      }
-    );
-  });
+  it('should reject a malformed identity');
 
-  it.only('should create a new identity', function(done) {
-    // console.log(config['identity-rest'].keys[0].privateKey.privateKeyPem);
-    // console.log(config['identity-rest'].keys[0].privateKey.publicKey);
-    var privateKeyPem =
-      config['identity-rest'].keys[0].privateKey.privateKeyPem;
-    var publicKeyId = config['identity-rest'].keys[0].privateKey.publicKey;
+});
+
+describe('admin users should update any identity', function() {
+  // console.log(config['identity-rest'].test.users.admin);
+  var userId = uuid.v4();
+  var idUrl = null;
+  var privateKeyPem =
+    config['identity-rest'].test.users.admin.keys.privateKey.privateKeyPem;
+  var publicKeyId =
+    config['identity-rest'].test.users.admin.keys.privateKey.publicKey;
+
+  before(function(done) {
     sendRequest(
       {
         url: identityService,
         method: 'POST',
-        identity: createIdentity('Abcdefg'),
+        identity: createIdentity(userId),
         privateKeyPem: privateKeyPem,
         publicKeyId: publicKeyId
-      },
-      function(err, res, body) {
-        // console.log('BODY', body.details.errors);
-        console.log('RETURNED BODY', body);
-        done(err);
-      }
-    );
+      }, function(err, res, body) {
+        idUrl = body.id;
+        done();
+      });
   });
+
+  it('should update an identities label', function(done) {
+    var newData = uuid.v4();
+    async.waterfall([
+      function(callback) {
+        sendRequest(
+          {
+            url: idUrl,
+            method: 'POST',
+            identity: {id: idUrl, label: newData},
+            privateKeyPem: privateKeyPem,
+            publicKeyId: publicKeyId
+          }, callback);
+      },
+      function(res, body, callback) {
+        res.statusCode.should.equal(204);
+        sendRequest(
+          {
+            url: idUrl,
+            method: 'GET'
+          }, callback);
+      },
+      function(res, body, callback) {
+        should.exist(body);
+        body.should.be.an('object');
+        should.exist(body.label);
+        body.label.should.be.a('string');
+        body.label.should.equal(newData);
+        callback();
+      }
+    ], function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
+  it('should update an identities description', function(done) {
+    var newData = uuid.v4();
+    async.waterfall([
+      function(callback) {
+        sendRequest(
+          {
+            url: idUrl,
+            method: 'POST',
+            identity: {id: idUrl, description: newData},
+            privateKeyPem: privateKeyPem,
+            publicKeyId: publicKeyId
+          }, callback);
+      },
+      function(res, body, callback) {
+        res.statusCode.should.equal(204);
+        sendRequest(
+          {
+            url: idUrl,
+            method: 'GET'
+          }, callback);
+      },
+      function(res, body, callback) {
+        should.exist(body);
+        body.should.be.an('object');
+        should.exist(body.label);
+        body.description.should.be.a('string');
+        body.description.should.equal(newData);
+        callback();
+      }
+    ], function(err) {
+      should.not.exist(err);
+      done();
+    });
+  });
+
 });
 
 function sendRequest(options, callback) {
