d5bfb001bc3ff8863b5180c577aae2de8b9a43a6
Matthew Collier <collier.matthew@gmail.com>
2016-09-21 10:10:05 -0400

    Update require statements.

--- test/test.js
+++ test/test.js
@@ -2,9 +2,6 @@
  * Copyright (c) 2015-2016 Digital Bazaar, Inc. All rights reserved.
  */
 var bedrock = require('bedrock');
-require('bedrock-express');
-require('bedrock-requirejs');
-require('bedrock-server');
 require('bedrock-identity-http');
 
 require('bedrock-test');
